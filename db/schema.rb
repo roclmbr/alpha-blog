a3e775eb3dd117ae6f68222fa3460f9b3daaf5cc
roclmbr <github@steveadamson.com>
2016-04-11 18:44:33 +0000

    Adding testing capability

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411141824) do
+ActiveRecord::Schema.define(version: 20160411170226) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160411141824) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
